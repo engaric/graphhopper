d18e5f1f10275f2d61e850da6c4be38be4b1f3a6
Stuart <engaric@hotmail.com>
2016-03-04 14:21:58 +0000

    Remove unused imports and modifiers implied by context. tab formatting uses spaces.

--- web/src/main/java/com/graphhopper/http/APIException.java
+++ web/src/main/java/com/graphhopper/http/APIException.java
@@ -4,23 +4,23 @@ import org.eclipse.jetty.http.HttpStatus;
 import org.eclipse.jetty.http.HttpStatus.Code;
 
 public class APIException extends Exception {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private HttpStatus.Code statusCode;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private HttpStatus.Code statusCode;
 
-	public APIException(Code statusCode, String message) {
-		super(message);
-		this.statusCode = statusCode;
-	}
-	
-	public HttpStatus.Code getStatusCode() {
-		return statusCode;
-	}
-	
-	public String getStatusMessage() {
-		return getMessage();
-	}
+    public APIException(Code statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public HttpStatus.Code getStatusCode() {
+        return statusCode;
+    }
+
+    public String getStatusMessage() {
+        return getMessage();
+    }
 
 }
