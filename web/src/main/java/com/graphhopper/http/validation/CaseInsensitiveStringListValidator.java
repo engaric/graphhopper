a35c4ce566487a38d297b7e9994883ddf556a0a2
Stuart <engaric@hotmail.com>
2016-03-04 14:04:38 +0000

    tab formatting uses spaces.

--- web/src/main/java/com/graphhopper/http/validation/CaseInsensitiveStringListValidator.java
+++ web/src/main/java/com/graphhopper/http/validation/CaseInsensitiveStringListValidator.java
@@ -3,22 +3,17 @@ package com.graphhopper.http.validation;
 import java.util.Arrays;
 import java.util.List;
 
-public class CaseInsensitiveStringListValidator
-{
-	public boolean isValid( String value, String... validValues )
-	{
-		return isValid(value, Arrays.asList(validValues));
-	}
+public class CaseInsensitiveStringListValidator {
+    public boolean isValid(String value, String... validValues) {
+        return isValid(value, Arrays.asList(validValues));
+    }
 
-	public boolean isValid( String value, List<String> validValues )
-	{
-		for (String validValue : validValues)
-		{
-			if (value.equalsIgnoreCase(validValue))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
+    public boolean isValid(String value, List<String> validValues) {
+        for (String validValue : validValues) {
+            if (value.equalsIgnoreCase(validValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
