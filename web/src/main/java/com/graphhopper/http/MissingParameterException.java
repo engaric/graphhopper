50fc23973c8abd72728bb98a078029add3ee7b8f
Stuart <engaric@hotmail.com>
2016-03-04 15:32:05 +0000

    Remove unused imports and modifiers implied by context. tab formatting uses spaces.

--- web/src/main/java/com/graphhopper/http/MissingParameterException.java
+++ web/src/main/java/com/graphhopper/http/MissingParameterException.java
@@ -4,15 +4,15 @@ import org.eclipse.jetty.http.HttpStatus;
 
 public class MissingParameterException extends APIException {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private static String message = "No %s parameter provided.";
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	public MissingParameterException(String parameter) {
-		super(HttpStatus.Code.BAD_REQUEST, String.format(message , parameter));
-	}
+    private static String message = "No %s parameter provided.";
+
+    public MissingParameterException(String parameter) {
+        super(HttpStatus.Code.BAD_REQUEST, String.format(message, parameter));
+    }
 
 }
--- web/src/main/java/com/graphhopper/http/NoSuchParameterException.java
+++ web/src/main/java/com/graphhopper/http/NoSuchParameterException.java
@@ -4,13 +4,13 @@ import org.eclipse.jetty.http.HttpStatus.Code;
 
 public class NoSuchParameterException extends APIException {
 
-	public NoSuchParameterException(String message) {
-		super(Code.BAD_REQUEST, message);
-	}
+    public NoSuchParameterException(String message) {
+        super(Code.BAD_REQUEST, message);
+    }
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
 }
