7087046e68495896717bee44557c329e9442df5d
engaric <engaric@hotmail.com>
2016-03-23 15:29:15 +0000

    Route-210: Implement single API end point for vehicular and non-vehicular routing.
    Update demo to use latest version of dte maps.

--- web/src/main/webapp/js/main-bng.js
+++ web/src/main/webapp/js/main-bng.js
@@ -515,7 +515,7 @@ function getTopLeftCorners() {
 
     var defaultLayer = baseMaps[selectLayer];
     if (!defaultLayer)
-        defaultLayer = osClassic;
+        defaultLayer = osRoad
 	var epsg27700 = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.999601 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894 +datum=OSGB36 +units=m +no_defs";    
     var crs = new L.Proj.WMTSCRS(
     		'EPSG:27700',
