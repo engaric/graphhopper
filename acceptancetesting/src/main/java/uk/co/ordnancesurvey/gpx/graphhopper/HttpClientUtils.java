f9d62cdea4240ab8d2f1c4c98474f334a1096718
sadam <engaric@hotmail.com>
2017-05-03 11:58:52 +0000

    imports

--- acceptancetesting/src/main/java/uk/co/ordnancesurvey/gpx/graphhopper/HttpClientUtils.java
+++ acceptancetesting/src/main/java/uk/co/ordnancesurvey/gpx/graphhopper/HttpClientUtils.java
@@ -23,6 +23,9 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HttpClientUtils {
     private static final String SSL_KEY_FILE = "sslKeyFile";
     private static final String SSL_KEY_PASS = "sslKeyPass";
